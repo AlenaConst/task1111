55066652a4fe339800b3c304f0598f06b4809f29
AlenaConst <65917187+AlenaConst@users.noreply.github.com>
2020-12-30 13:39:46 +0300

    Add files via upload

---
+++ include/graph.h
@@ -0,0 +1,20 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+#include "draw.h"
+const int nn=5;
+class graph:public draw
+{
+    public:
+        graph();
+        graph(int n,int* m);
+        virtual ~graph();
+        int Getnum(){return numb;}
+        void Setnum(int n){numb=n;}
+        void show();
+        void Floyd(int i1, int j1);
+    private:
+        int numb;
+        int* ms;
+};
+
+#endif // GRAPH_H
